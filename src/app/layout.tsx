c647303de735c1bc60dbdc8c923cceaa79193777
yahya-n1 <naway@farmingdale.edu>
2026-05-12 08:35:44 -0400

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/ui/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import LayoutWrapper from "@/components/ui/layout-wrapper";
 
+export const metadata: Metadata = {
+  title: "HashBot AI", 
+  description: "HashBot AI - Use AI technology to generate images or chat to AI for help.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <Header />
+          <LayoutWrapper>{children}</LayoutWrapper>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
